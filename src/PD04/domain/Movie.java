5f7db0185f7d4f616fcc91d45412834b7ec3ce37
Czupson <mateusz300102@gmail.com>
2026-05-05 20:44:03 +0200

    feat: record Movie

--- src/PD04/domain/Movie.java
+++ src/PD04/domain/Movie.java
@@ -1,4 +1,24 @@
 package PD04.domain;
 
-public class Movie {
+public record Movie(
+        int id,
+        String title,
+        String director,
+        int year,
+        Category category
+){
+    public Movie {
+        if (title == null || title.isBlank()){
+            throw new IllegalArgumentException("title cannot be blank");
+        }
+        if (director == null || director.isBlank()){
+            throw new IllegalArgumentException("director cannot be blank");
+        }
+        if (category == null){
+            throw new IllegalArgumentException("category cannot be null");
+        }
+        if (year < 1888) {
+            throw new IllegalArgumentException("year must be >= 1888");
+        }
+    }
 }
