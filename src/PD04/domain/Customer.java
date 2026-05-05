5d1c21964bd01c263a676f92d0d5c03c2020ad86
Czupson <mateusz300102@gmail.com>
2026-05-05 20:47:43 +0200

    feat: record Customer

--- src/PD04/domain/Customer.java
+++ src/PD04/domain/Customer.java
@@ -1,4 +1,24 @@
 package PD04.domain;
 
-public class Customer {
+public record Customer(
+        int id,
+        String firstName,
+        String lastName,
+        int age,
+        CustomerStatus status
+) {
+    public Customer {
+        if (firstName == null || firstName.isBlank()) {
+            throw new IllegalArgumentException("first name cannot be blank");
+        }
+        if (lastName == null || lastName.isBlank()) {
+            throw new IllegalArgumentException("last name cannot be blank");
+        }
+        if (age < 0 || age > 130) {
+            throw new IllegalArgumentException("age must be between 0 and 130");
+        }
+        if (status == null) {
+            throw new IllegalArgumentException("status cannot be null");
+        }
+    }
 }
